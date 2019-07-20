e0673a06034b4f3707ba1c0ec7045242a04e6c44
Kerry Shetline <kerry@shetline.com>
2019-07-20 00:53:14 -0400

    After struggling with trying to get rotating-file-stream to do what I thought it would automatically do for me, take over handling the stuff it doesn't do.

--- app/svc-api-logger.ts
+++ app/svc-api-logger.ts
@@ -1,36 +1,88 @@
 import rfs from 'rotating-file-stream';
 import { join as pathJoin } from 'path';
 import stream, { Writable } from 'stream';
-import { WriteStream } from 'fs';
+import fs, { WriteStream } from 'fs';
 import { Request, Response } from 'express';
 import * as util from 'util';
-import { formatDateTime } from 'ks-util';
+import { formatDateTime, zeroPad } from 'ks-util';
 
 export let svcApiLogStream: Writable | WriteStream = process.stdout;
 
+const INTERVAL_DAYS = 7;
+const MAX_FILES = 10;
+
 if (process.env.SVC_API_LOG) {
   const options: any = {
-    interval: '7d',
-    maxFiles: 10,
+    interval: INTERVAL_DAYS + 'd',
+    maxFiles: MAX_FILES,
     maxSize: '256K',
     rotationTime: true
   };
+  const fullLogPath = pathJoin(__dirname, process.env.SVC_API_LOG);
+  const intervalMillis = INTERVAL_DAYS * 86400000;
+  let logCreation = fs.existsSync(fullLogPath) && fs.statSync(fullLogPath).birthtimeMs;
   let logPath: string;
-  let logFile = pathJoin(__dirname, process.env.SVC_API_LOG);
-  const $ = /(.*)[\/\\](.+)/.exec(logFile);
+  let logFile = fullLogPath;
+  let ext = '';
+  let separator = '';
+  let $ = /(.*)([\/\\])(.+)/.exec(logFile);
 
   if ($) {
     logPath = $[1];
-    logFile = $[2];
+    separator = $[2];
+    logFile = $[3];
 
     if (logPath)
       options.path = logPath;
   }
 
-  const fileStream = rfs(logFile, options);
+  if (($ = /(.*)(\..*)/.exec(logFile))) {
+    logFile = $[1];
+    ext = $[2];
+  }
+
+  const fileName = (time: Date, index: number): string => {
+    if (!time)
+      return logFile + ext;
+    else {
+      const date = new Date(+time - time.getTimezoneOffset() * 60000);
+
+      return `${logFile}-${date.toISOString().substr(0, 10)}_${zeroPad(index, 2)}${ext}`;
+    }
+  };
+
+  let fileStream = rfs(fileName, options);
 
   svcApiLogStream = new stream.Writable();
-  svcApiLogStream._write = (chunk: any, encoding: string, done: (error?: Error) => void) => {
+  svcApiLogStream._write = async (chunk: any, encoding: string, done: (error?: Error) => void) => {
+    // rotating-file-stream isn't doing as complete a job as I'd hope for in rotating file names
+    // and cleaning up files, so the code below takes over some of that work.
+    const now = Date.now();
+    let checkMaxFiles = false;
+
+    if (logCreation && now > logCreation + intervalMillis) {
+      const nowDate = new Date(now);
+      let index = 0;
+      let datedFilePath: string;
+
+      do {
+        datedFilePath = logPath + separator + fileName(nowDate, index++);
+      } while (fs.existsSync(datedFilePath));
+
+      if (fileStream.close)
+        fileStream.close();
+      else if (fileStream.end) {
+        await new Promise(resolve => {
+          fileStream.end(() => resolve());
+        });
+      }
+
+      logCreation = now;
+      fs.renameSync(fullLogPath, datedFilePath);
+      fileStream = rfs(fileName, options);
+      checkMaxFiles = true;
+    }
+
     let output: any;
 
     if (chunk instanceof Buffer) {
@@ -46,7 +98,16 @@ if (process.env.SVC_API_LOG) {
       output = chunk.toString();
 
     try {
-      fileStream.write(output);
+      fileStream.write(output, () => {
+        if (checkMaxFiles) {
+          const files = fs.readdirSync(logPath).sort().filter(name => name.startsWith(logFile + '-'));
+
+          while (files.length > MAX_FILES - 1) {
+            fs.unlinkSync(logPath + separator + files[0]);
+            files.splice(0, 1);
+          }
+        }
+      });
     }
     catch (err) { /* ignore errors writing to log file */ }
 
