792c726d835c18ad468d579b2a6fde0f4ed58fe6
Kerry Shetline <kshetline@campsystems.com>
2022-04-04 15:22:49 -0400

    Allow most zone-for-location queries to be done with my own DB instead of a Google API call.

--- app/zone-for-location.ts
+++ app/zone-for-location.ts
@@ -2,17 +2,58 @@ import { Request, Response, Router } from 'express';
 import { toBoolean, toNumber } from '@tubular/util';
 import { asyncHandler, processMillis } from './common';
 import { requestJson } from 'by-request';
+import { pool } from './atlas_database';
 
 export const router = Router();
 
-router.get('/', asyncHandler(async (req: Request, res: Response) => {
-  const lat = toNumber(req.query.lat);
-  const lon = toNumber(req.query.lon);
-  const time = toNumber(req.query.timestamp, Math.floor(processMillis() / 1000));
+export interface TzInfo {
+  country?: string;
+  dstOffset?: number;
+  errorMessage?: string;
+  fromDb?: boolean;
+  rawOffset?: number;
+  status?: string;
+  timeZoneId?: string;
+  timeZoneName?: string;
+}
+
+export async function getTimezoneForLocation(lat: number, lon: number, time = 0): Promise<TzInfo> {
+  if (time === 0)
+    time = Math.floor(processMillis() / 1000);
+
+  try {
+    const connection = await pool.getConnection();
+
+    zoneLoop:
+    for (const span of [0.05, 0.1, 0.25, 0.5]) {
+      const query = 'SELECT time_zone, country FROM atlas2 WHERE latitude >= ? AND latitude <= ? AND longitude >= ? AND longitude <= ?';
+      const results = (await connection.queryResults(query, [lat - span, lat + span, lon - span, lon + span])) || [];
+      let timeZoneName: string;
+      let country: string;
+
+      for (const result of results) {
+        if (result.time_zone) {
+          if (!timeZoneName)
+            timeZoneName = result.time_zone;
+          else if (timeZoneName !== result.time_zone)
+            break zoneLoop;
+
+          if (!country)
+            country = result.country;
+          else if (country !== result.country)
+            break zoneLoop;
+        }
+      }
+
+      if (timeZoneName)
+        return { timeZoneName, country, status: 'OK', fromDb: true };
+    }
+  }
+  catch {}
+
   const key = encodeURIComponent(process.env.GOOGLE_API_KEY);
-  const plainText = toBoolean(req.query.pt, false, true);
   const url = `https://maps.googleapis.com/maps/api/timezone/json?location=${lat},${lon}&timestamp=${time}&key=${key}`;
-  let data: any;
+  let data: TzInfo;
 
   try {
     data = await requestJson(url);
@@ -20,6 +61,16 @@ router.get('/', asyncHandler(async (req: Request, res: Response) => {
   catch (err) {
     data = { status: 'ERROR', errorMessage: err.toString() };
   }
+
+  return data;
+}
+
+router.get('/', asyncHandler(async (req: Request, res: Response) => {
+  const lat = toNumber(req.query.lat);
+  const lon = toNumber(req.query.lon);
+  const time = toNumber(req.query.timestamp);
+  const plainText = toBoolean(req.query.pt, false, true);
+  const data = await getTimezoneForLocation(lat, lon, time);
 
   if (plainText) {
     res.set('Content-Type', 'text/plain');
